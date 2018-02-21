0ce91d01a2852ba1d874c62f5949a14646820ff3
Alvin Ayusa <alvinayusa@alvins-mbp.lan>
2018-02-20 21:07:42 -0800

    0220 2107

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,77 +6,103 @@ $(document).ready(function(){
 var questions = [
     
     {
-        quest01 : "Which company used to bear the name: Research in Motion?",
+        question : "Which company used to bear the name: Research in Motion?",
         choices : ["Nokia", "Blackberry", "Apple", "Sprint"],
-        answer : 1,
+        values : [false, true, false, false],
         trivia : "Research in Motion became Blackberry in 2013."
     },
     {
-        quest02 : "Which company used to bear the name: Tokyo Tshushin Kogyo?",
+        question : "Which company used to bear the name: Tokyo Tshushin Kogyo?",
         choices : ["Nintendo", "LG", "Sony", "Samsung"],
-        answer : 2,
+        answer : [false, false, true, false],
         trivia : "Tokyo Tsushin Kogyo was founded in 1946 the company is now known as Sony"
     },
     {
-        quest03 : "Which company used to bear the name: AuctionWeb?",
+        question : "Which company used to bear the name: AuctionWeb?",
         choices : ["Amazon", "Craigslist", "Paypal", "eBay"],
-        answer : 3,
+        answer : [false, false, false, true],
         trivia : "AuctionWeb was founded in 1995 and is now known as eBay"
     },
     {
-        quest04 : "Which company used to bear the name: Cadabra?",
+        question : "Which company used to bear the name: Cadabra?",
         choices : ["Amazon", "Apple", "Target", "eBay"],
-        answer : 0,
+        answer : [true, false, false, false],
         trivia : "Jeff Bezos originally called it Cadabra, changing it to Amazon in 1995."
     },
     {
-        quest05 : "Which company used to bear the name: BackRub?",
+        question : "Which company used to bear the name: BackRub?",
         choices : ["Samsung", "LG", "Google", "Walmart"],
-        answer : 2,
+        answer : [false, false, true, false],
         trivia : "The first search engine Larry Page and Sergey Brin worked on was called BackRub, before becoming Google.com in 1997."
     },
     {
-        quest06 : "Which company used to bear the name: Jerry and David’s Guide to the World Wide Web?",
+        question : "Which company used to bear the name: Jerry and David’s Guide to the World Wide Web?",
         choices : ["Microsoft", "Netscape", "Yahoo", "AOL"],
-        answer : 2,
+        answer : [false, false, true, false],
         trivia : "The name of a website created in 1994 by Jerry Yang and David Filo eventually became Yahoo in April 1994."
     },
     {
-        quest07 : "Which company used to bear the name: Chinese Academy of Sciences Computer Technology Research Institute New Technology Development Company?",
+        question : "Which company used to bear the name: Chinese Academy of Sciences Computer Technology Research Institute New Technology Development Company?",
         choices : ["Alibaba", "Lenovo", "Dell", "IBM"],
-        answer : 1,
+        answer : [false, true, false, false],
         trivia : "It’s hard to believe, but at their first meeting in 1984, the founders of Lenovo agreed to call it ‘Chinese Academy of Sciences Computer Technology Research Institute New Technology Development Company’"
     },
     {
-        quest08 : "Which company used to bear the name: Computing-Tabulating-Recording Company?",
+        question : "Which company used to bear the name: Computing-Tabulating-Recording Company?",
         choices : ["Apple", "Microsoft", "Oracle", "IBM"],
-        answer : 3,
+        answer : [false, false, false, true],
         trivia : "The original name of IBM is Computing-Tabulating-Recording Company.  It was in 1924 when it changed its name to IBM."
     },
     {
-        quest09 : "Which company used to bear the name: Sound of Music?",
+        question : "Which company used to bear the name: Sound of Music?",
         choices : ["BestBuy", "Target", "Sony", "Samsung"],
-        answer : 0,
+        answer : [true, false, false, false],
         trivia : "Buy’s original name is Sound of Music.  It was founded in 1966 by Richard Schulze as a home and car stereo store in St. Paul, Minn."
     },
     {
-        quest10 : "Which company used to bear the name: Quantum Computer Services?",
+        question : "Which company used to bear the name: Quantum Computer Services?",
         choices : ["Sony", "Nokia", "AOL", "Salesforce"],
-        answer : 2,
+        answer : [false, false, true, false],
         trivia : "The original name of AOL is Quantum Computer Services. It was started in 1983."
     }
 ]
 
-var displayed = #questSection
+// list of global variables for tracking of scores
+var currentQuestion = 0
+var currentOptions = 0
+var correct = 0
+var wrong = 0 
+var unanswered = 0
+// end of global variables for tracking scores
+
+
+
+// Display my question in the question section.
+// Display the list of answers on the screen using for loop.
+for (var i = 0; i < questions[currentQuestion].choices.length; i++){
+    $("#questionSection").html(questions[i].question)
+
+    for (var j = 0; j < 4; j++){
+        $("#option-" + i).append(questions[i].choices[j])
+        
+    }
+
+console.log(questions[i])
+}
+
+
+
+
 
 // click a button to start a Game
 function startGame(){
     // one question at a time will be shown on the screen
+
     for (var i =0; i < questions.length; i++){
-        $("#questSection").append(displayed) 
+        
     }
 }
-console.log(startGame())
+// console.log(startGame())
 
 // only one of the possible answers can be chosen
 // a timer per question is presented
